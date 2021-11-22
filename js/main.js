1c1fb29115af8dca78f94f69e80a5336f8a93132
Matt Graham <iam@mattgrah.am>
2021-11-22 20:28:15 +1100

    refactoring code for click function and removed redundtant css

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@
 let player1 = 0;
 let player2 = 0;
 let currentPlayer = "Player 1";
+let nextPlayer = "Player 2";
 let winner = false;
+let symbol = "X";
 let playWindow = document.querySelector(".play_window");
 let resetBtn = document.getElementById("reset");
 let playAgainBtn = document.getElementById("play_again");
@@ -37,23 +39,20 @@ playWindow.addEventListener("mousedown", function (event) {
 
 // Set X or O Function
 function clickXO(event) {
-    if (event.target.innerText !== "X" && event.target.innerText !== "O") {
-        if (currentPlayer === "Player 1") {
-            event.target.classList.add("player1");
-            event.target.innerText = "X";
-            playerTurn.classList.add("player2");
-            playerTurn.classList.remove("player1");
-            currentPlayer = "Player 2";
-            playerTurn.innerText = `${currentPlayer}'s Turn`;
-        } else if (currentPlayer === "Player 2") {
-            event.target.classList.add("player2");
-            event.target.innerText = "O";
-            playerTurn.classList.add("player1");
-            playerTurn.classList.remove("player2");
-            currentPlayer = "Player 1";
-            playerTurn.innerText = `${currentPlayer}'s Turn`;
-        }
+    if (event.target.innerText === "") {
+        return currentPlayer === "Player 1"
+            ? clickPlayer("Player 1", event)
+            : clickPlayer("Player 2", event);
     }
+}
+
+// function for each player styling.
+function clickPlayer(player, event) {
+    event.target.style.color = player === "Player 1" ? "hsl(355, 78%, 56%)" : "hsl(203, 39%, 44%)";
+    event.target.innerText = player === "Player 1" ? "X" : "O";
+    playerTurn.style.color = player === "Player 1" ? "hsl(203, 39%, 44%)" : "hsl(355, 78%, 56%)";
+    currentPlayer = player === "Player 1" ? "Player 2" : "Player 1";
+    playerTurn.innerText = `${player}'s Turn`;
 }
 
 // Check for Win Condition
@@ -72,21 +71,19 @@ function winCheck(event) {
                 return;
             } else if (playWindow.children[a].innerText === "X") {
                 playerTurn.innerText = `Player 1 Wins`;
-                playerTurn.classList.add("player1");
-                playerTurn.classList.remove("player2");
-                playWindow.children[a].classList.add("player1_win");
-                playWindow.children[b].classList.add("player1_win");
-                playWindow.children[c].classList.add("player1_win");
+                playerTurn.style.color = "hsl(355, 78%, 56%)";
+                playWindow.children[a].classList.add("player1");
+                playWindow.children[b].classList.add("player1");
+                playWindow.children[c].classList.add("player1");
                 player1++;
                 document.querySelector("#player1_score").innerText = player1;
                 winner = true;
             } else if (playWindow.children[a].innerText === "O") {
                 playerTurn.innerText = `Player 2 Wins`;
-                playerTurn.classList.add("player2");
-                playerTurn.classList.remove("player1");
-                playWindow.children[a].classList.add("player2_win");
-                playWindow.children[b].classList.add("player2_win");
-                playWindow.children[c].classList.add("player2_win");
+                playerTurn.style.color = "hsl(203, 39%, 44%)";
+                playWindow.children[a].classList.add("player2");
+                playWindow.children[b].classList.add("player2");
+                playWindow.children[c].classList.add("player2");
                 player2++;
                 document.querySelector("#player2_score").innerText = player2;
                 winner = true;
@@ -99,16 +96,18 @@ function winCheck(event) {
 function drawCheck() {
     if (currentGame.length >= 9 && winner === false) {
         playerTurn.innerText = "It's a Tie!";
-        playerTurn.classList.remove("player1", "player2");
+        playerTurn.style.color = "hsl(215, 50%, 23%)";
     }
 }
 
 // Function for both playagain and reset btn to clear board.
 function boardClear() {
     item.innerText = "";
-    item.classList.remove("player1_win", "player2_win", "player1", "player2");
+    item.classList.remove("player1", "player2");
     winner = false;
     currentGame = [];
+    playerTurn.innerText = `Begin Game`;
+    playerTurn.style.color = "hsl(215, 50%, 23%)";
 }
 
 // Play again Button (retain scores)
